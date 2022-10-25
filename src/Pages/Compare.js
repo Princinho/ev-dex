6b73960761acd8386ad4d8717b1f7cc686f891cf
Princinho <princegnakou@gmail.com>
2022-10-25 07:10:53 +0000

    add search bar to compare

--- src/Pages/Compare.js
+++ src/Pages/Compare.js
@@ -3,16 +3,17 @@ import React from "react";
 export default function Compare({ carManager }) {
     const [index1, setIndex1] = React.useState(0)
     const [index2, setIndex2] = React.useState(0)
+    const [search, setSearch] = React.useState("")
     const [v1, setV1] = React.useState(null)
     const [v2, setV2] = React.useState(null)
+    const [vehicles, setVehicles] = React.useState(carManager.vehiclesData)
     React.useEffect(() => {
-        setV1(carManager.vehiclesData[index1])
-        setV2(carManager.vehiclesData[index2])
-        console.log("Effects")
+        setV1(vehicles[index1])
+        setV2(vehicles[index2])
     })
     function getMax(key) {
-        if (!carManager.vehiclesData) return 0
-        return carManager.vehiclesData.reduce((prev, current) => {
+        if (!vehicles) return 0
+        return vehicles.reduce((prev, current) => {
             return parseInt(prev) < parseInt(current[key]) ? current[key] : prev
 
         }, 0)
@@ -43,26 +44,37 @@ export default function Compare({ carManager }) {
             </div>
         )
     }
+
     function getWidth(vehicle, key) {
         if (key == 'accelerationSec') {
             const result = (parseFloat(getMax(key)) - parseFloat(vehicle[key])) / parseFloat(getMax(key)) * 100 + "%"
-            console.log(getMax(key))
-            console.log(result)
             return result
         }
         return parseInt(vehicle[key]) / parseInt(getMax(key)) * 100 + "%"
     }
+    console.log(vehicles)
+    function updateSearch(e) {
+        const searchTerm = e.target.value
+        const filteredVehicles=carManager.vehiclesData.filter(v =>
+            v.model.toLowerCase().includes(search.toLowerCase())
+            || v.brand.toLowerCase().includes(search.toLowerCase()))
+        setSearch(searchTerm)
+        setVehicles(searchTerm==""?carManager.vehiclesData:filteredVehicles)
+        setIndex1(0)
+        setIndex2(0)
 
+    }
     return (
-        <div className="section-compare">
+        <section className="section-compare">
+            <input type="text" name="search" onChange={updateSearch} value={search} className="search" style={{ width: "100%" }} placeholder="Search among 100+ EVs" />
             <div style={{ display: "flex", justifyContent: "space-around" }}>
-                <VehiclePicker vehiclesData={carManager.vehiclesData} selectedIndex={index1} setSelected={(index) => {
+                <VehiclePicker vehiclesData={vehicles} selectedIndex={index1} setSelected={(index) => {
                     setIndex1(index)
-                    setV1(carManager.vehiclesData[index])
+                    setV1(vehicles[index])
                 }} />
-                <VehiclePicker vehiclesData={carManager.vehiclesData} selectedIndex={index2} setSelected={(index) => {
+                <VehiclePicker vehiclesData={vehicles} selectedIndex={index2} setSelected={(index) => {
                     setIndex2(index)
-                    setV2(carManager.vehiclesData[index])
+                    setV2(vehicles[index])
                 }} />
             </div>
             {(v2 && v1) &&
@@ -101,6 +113,6 @@ export default function Compare({ carManager }) {
 
 
                 </div>}
-        </div>
+        </section>
     )
 }
